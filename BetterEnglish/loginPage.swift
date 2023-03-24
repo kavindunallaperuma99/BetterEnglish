5f754fb7b9aa1da5ea0fd825d2f0a459ed4c8486
Kavindu Nallaperuma <kavindu.nallaperuma@gmail.com>
2023-03-24 20:57:52 +0530

    Login Stage 1 -  Firebase User Authentication

--- BetterEnglish/loginPage.swift
+++ BetterEnglish/loginPage.swift
@@ -12,13 +12,12 @@ struct loginPage: View {
     @State var email: String = ""
     @State var password: String = ""
     @State var isLoggingIn: Bool = false
+    @State var errorMessage: String = ""
     
     var body: some View {
         
         NavigationView {
-
             VStack {
-           
                 Text("Log in")
                     .padding(.top, 50)
                     .frame(maxWidth: .infinity, alignment: .leading)
@@ -32,7 +31,6 @@ struct loginPage: View {
                     .padding(.all, 12.0)
                     .padding(.top, -10)
                     .padding(.bottom, -30)
-                
                 
                 TextField("Email", text: $email)
                     .padding(.all)
@@ -48,7 +46,6 @@ struct loginPage: View {
                     .padding(.top, -10)
                     .padding(.bottom, -30)
                 
-                
                 SecureField("Password", text: $password)
                     .padding(.all)
                     .background(Color.white)
@@ -57,57 +54,48 @@ struct loginPage: View {
                     .shadow(radius: 2)
                     .padding(.all, 12.0)
                 
-                
-                Text("Forgot password?")
-                    .frame(maxWidth: .infinity, alignment: .leading)
+                Text(errorMessage)
+                    .foregroundColor(.red)
+                    .fixedSize(horizontal: false, vertical: true)
                     .padding(.all, 12.0)
-                    .padding(.top, -30)
-                    .padding(.bottom, 60)
                 
-                /*NavigationLink(destination: ContentView()) {
-                    Button  {
-                        } label: {
-                        Text("Log in")
-                                .foregroundColor(.white)
-                                .fontWeight(.bold)
-                                .padding()
+                Button(action: {
+                    // Authenticate the user with Firebase
+                    Auth.auth().signIn(withEmail: email, password: password) { result, error in
+                        if let error = error {
+                            errorMessage = error.localizedDescription
+                        } else {
+                            UIApplication.shared.windows.first?.rootViewController = UIHostingController(rootView: ContentView())
+                            UIApplication.shared.windows.first?.makeKeyAndVisible()
                         }
-                        .frame(maxWidth: .infinity, alignment: .center)
-                        .background(Color.black)
-                        .cornerRadius(5.0)
-                        .shadow(radius: 2)
-                        .padding(.all, 12.0)
-                    
-                }*/
-                
-                NavigationLink(destination: ContentView() ) {
+                    }
+                }) {
                     Text("Log in")
                         .foregroundColor(.white)
-                        .frame(maxWidth: .infinity, alignment: .center)
-                        .frame(height: 55)
-                        .background(Color.black)
-                        .cornerRadius(5.0)
-                        .shadow(radius: 2)
-                        .padding(.all, 12.0)
+                        .fontWeight(.bold)
+                        .padding()
                 }
+                .frame(maxWidth: .infinity, alignment: .center)
+                .background(Color.black)
+                .cornerRadius(5.0)
+                .shadow(radius: 2)
+                .padding(.all, 12.0)
                 
-               NavigationLink(destination: signupPage() ){
-                    Text("Don't you have an account?")
+                NavigationLink(destination: signupPage()) {
+                    Text("Don't have an account?")
                         .frame(maxWidth: .infinity, alignment: .leading)
                         .foregroundColor(.black)
                         .padding(.all, 12.0)
                         .padding(.top, -25)
                         .padding(.bottom, 60)
-                } 
+                }
                 
                 Text("Or login with")
                     .frame(maxWidth: .infinity, alignment: .center)
                     .padding(.bottom, 30)
                 
-                
-                HStack{
+                HStack {
                     Button(action: {
-                        
                         // Handle Google authentication
                     }) {
                         Image("googleLogo")
